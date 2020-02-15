76d0e67d7a536a74aacc2ec3535354686902d5e5
Edgeoutrock <phana@mymail.vcu.edu>
2020-02-15 12:35:10 -0500

    for heroku database

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,20 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Freedom123!",
+  password: "",
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  })
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
